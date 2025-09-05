448d7ca9ab6e82f409356479a63286512785e3e7
ryfazrin <riansera515@gmail.com>
2025-09-05 11:29:34 +0800

    fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ryfazrin.github.io",
-  base: '/GootaDocs',
+  base: '/GootaDocs/',
   markdown: {
     smartypants: true,
     syntaxHighlight: "shiki",
